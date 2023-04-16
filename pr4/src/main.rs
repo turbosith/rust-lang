bf4840a812e8fd399a8a0c6e57845c5792eec353
Evdokimov <danila.evdokimov.444@mail.ru>
2023-04-16 20:26:01 +0300

    Practical work 4

---
+++ pr4/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let my_string = String::from("hello world");
+    let word = first_word(&my_string[0..6]);
+    println!("{word}");
+    let word = first_word(&my_string[..]);
+    println!("{word}");
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    let word = first_word(my_string_literal);
+}
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
